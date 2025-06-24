864b14eb62d05372a978e6da4d460d68203e04e9
Phan VT <phanvantam0112@gmail.com>
2025-06-24 16:11:04 +0700

    django authentication

--- greatkart/settings.py
+++ greatkart/settings.py
@@ -133,3 +133,17 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
+
+#SMTP configuration
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'phanvantam0112@gmail.com'
+EMAIL_HOST_PASSWORD = 'iwqpiezlyjyipsat' 
